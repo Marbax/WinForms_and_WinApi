7d743590334cd5a7e8a5363c64f4df75b55d59a7
Marbax <40849771+Marbax@users.noreply.github.com>
2020-02-14 09:36:17 +0200

    hw_03_completed
    app for cakc time span between two dates in different date types

---
+++ HW/HW_03/time_span_calc/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace time_span_calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonShowInfo = new System.Windows.Forms.Button();
+            this.radioButtonSeconds = new System.Windows.Forms.RadioButton();
+            this.radioButtonMinutes = new System.Windows.Forms.RadioButton();
+            this.radioButtonDays = new System.Windows.Forms.RadioButton();
+            this.radioButtonMonths = new System.Windows.Forms.RadioButton();
+            this.radioButtonYears = new System.Windows.Forms.RadioButton();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.labelPeekDate = new System.Windows.Forms.Label();
+            this.textBoxInputEndDate = new System.Windows.Forms.TextBox();
+            this.labelInputDate = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.timerChekAndRun = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonShowInfo
+            // 
+            this.buttonShowInfo.Location = new System.Drawing.Point(491, -1);
+            this.buttonShowInfo.Name = "buttonShowInfo";
+            this.buttonShowInfo.Size = new System.Drawing.Size(26, 24);
+            this.buttonShowInfo.TabIndex = 7;
+            this.buttonShowInfo.Text = "?";
+            this.buttonShowInfo.UseVisualStyleBackColor = true;
+            this.buttonShowInfo.Click += new System.EventHandler(this.buttonShowInfo_Click);
+            // 
+            // radioButtonSeconds
+            // 
+            this.radioButtonSeconds.AutoSize = true;
+            this.radioButtonSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonSeconds.Location = new System.Drawing.Point(349, 116);
+            this.radioButtonSeconds.Name = "radioButtonSeconds";
+            this.radioButtonSeconds.Size = new System.Drawing.Size(167, 24);
+            this.radioButtonSeconds.TabIndex = 4;
+            this.radioButtonSeconds.TabStop = true;
+            this.radioButtonSeconds.Text = "Convert to Seconds";
+            this.radioButtonSeconds.UseVisualStyleBackColor = true;
+            this.radioButtonSeconds.CheckedChanged += new System.EventHandler(this.radioButtonSeconds_CheckedChanged);
+            // 
+            // radioButtonMinutes
+            // 
+            this.radioButtonMinutes.AutoSize = true;
+            this.radioButtonMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonMinutes.Location = new System.Drawing.Point(349, 93);
+            this.radioButtonMinutes.Name = "radioButtonMinutes";
+            this.radioButtonMinutes.Size = new System.Drawing.Size(160, 24);
+            this.radioButtonMinutes.TabIndex = 3;
+            this.radioButtonMinutes.TabStop = true;
+            this.radioButtonMinutes.Text = "Convert to Minutes";
+            this.radioButtonMinutes.UseVisualStyleBackColor = true;
+            this.radioButtonMinutes.CheckedChanged += new System.EventHandler(this.radioButtonMinutes_CheckedChanged);
+            // 
+            // radioButtonDays
+            // 
+            this.radioButtonDays.AutoSize = true;
+            this.radioButtonDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonDays.Location = new System.Drawing.Point(349, 70);
+            this.radioButtonDays.Name = "radioButtonDays";
+            this.radioButtonDays.Size = new System.Drawing.Size(140, 24);
+            this.radioButtonDays.TabIndex = 2;
+            this.radioButtonDays.TabStop = true;
+            this.radioButtonDays.Text = "Convert to Days";
+            this.radioButtonDays.UseVisualStyleBackColor = true;
+            this.radioButtonDays.CheckedChanged += new System.EventHandler(this.radioButtonDays_CheckedChanged);
+            // 
+            // radioButtonMonths
+            // 
+            this.radioButtonMonths.AutoSize = true;
+            this.radioButtonMonths.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonMonths.Location = new System.Drawing.Point(349, 47);
+            this.radioButtonMonths.Name = "radioButtonMonths";
+            this.radioButtonMonths.Size = new System.Drawing.Size(152, 24);
+            this.radioButtonMonths.TabIndex = 1;
+            this.radioButtonMonths.TabStop = true;
+            this.radioButtonMonths.Text = "Conver to Months";
+            this.radioButtonMonths.UseVisualStyleBackColor = true;
+            this.radioButtonMonths.CheckedChanged += new System.EventHandler(this.radioButtonMonthes_CheckedChanged);
+            // 
+            // radioButtonYears
+            // 
+            this.radioButtonYears.AutoSize = true;
+            this.radioButtonYears.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonYears.Location = new System.Drawing.Point(349, 24);
+            this.radioButtonYears.Name = "radioButtonYears";
+            this.radioButtonYears.Size = new System.Drawing.Size(146, 24);
+            this.radioButtonYears.TabIndex = 0;
+            this.radioButtonYears.TabStop = true;
+            this.radioButtonYears.Text = "Convert to Years";
+            this.radioButtonYears.UseVisualStyleBackColor = true;
+            this.radioButtonYears.CheckedChanged += new System.EventHandler(this.radioButtonYears_CheckedChanged);
+            // 
+            // dateTimePickerEndDate
+            // 
+            this.dateTimePickerEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePickerEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(13, 23);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(311, 26);
+            this.dateTimePickerEndDate.TabIndex = 1;
+            this.dateTimePickerEndDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult.Location = new System.Drawing.Point(13, 129);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(311, 26);
+            this.textBoxResult.TabIndex = 2;
+            // 
+            // labelPeekDate
+            // 
+            this.labelPeekDate.AutoSize = true;
+            this.labelPeekDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPeekDate.Location = new System.Drawing.Point(12, 0);
+            this.labelPeekDate.Name = "labelPeekDate";
+            this.labelPeekDate.Size = new System.Drawing.Size(115, 20);
+            this.labelPeekDate.TabIndex = 3;
+            this.labelPeekDate.Text = "Peek end Date";
+            // 
+            // textBoxInputEndDate
+            // 
+            this.textBoxInputEndDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInputEndDate.Location = new System.Drawing.Point(13, 73);
+            this.textBoxInputEndDate.Name = "textBoxInputEndDate";
+            this.textBoxInputEndDate.Size = new System.Drawing.Size(311, 26);
+            this.textBoxInputEndDate.TabIndex = 4;
+            this.textBoxInputEndDate.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // labelInputDate
+            // 
+            this.labelInputDate.AutoSize = true;
+            this.labelInputDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInputDate.Location = new System.Drawing.Point(12, 52);
+            this.labelInputDate.Name = "labelInputDate";
+            this.labelInputDate.Size = new System.Drawing.Size(135, 20);
+            this.labelInputDate.TabIndex = 5;
+            this.labelInputDate.Text = "Or input end Date";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(135, 105);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(55, 20);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Result";
+            // 
+            // timerChekAndRun
+            // 
+            this.timerChekAndRun.Tick += new System.EventHandler(this.timerChekAndRun_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 156);
+            this.Controls.Add(this.buttonShowInfo);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.radioButtonSeconds);
+            this.Controls.Add(this.labelInputDate);
+            this.Controls.Add(this.radioButtonMinutes);
+            this.Controls.Add(this.textBoxInputEndDate);
+            this.Controls.Add(this.radioButtonDays);
+            this.Controls.Add(this.labelPeekDate);
+            this.Controls.Add(this.radioButtonMonths);
+            this.Controls.Add(this.radioButtonYears);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.dateTimePickerEndDate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButtonYears;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
+        private System.Windows.Forms.RadioButton radioButtonSeconds;
+        private System.Windows.Forms.RadioButton radioButtonMinutes;
+        private System.Windows.Forms.RadioButton radioButtonDays;
+        private System.Windows.Forms.RadioButton radioButtonMonths;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label labelPeekDate;
+        private System.Windows.Forms.TextBox textBoxInputEndDate;
+        private System.Windows.Forms.Label labelInputDate;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonShowInfo;
+        private System.Windows.Forms.Timer timerChekAndRun;
+    }
+}
+
---
+++ HW/HW_03/time_span_calc/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace time_span_calc
+{
+
+    /// <summary>
+    ///  Написать программу, вычисляющую сколько осталось времени до указанной даты (дата вводится с клавиатуры в Edit). 
+    ///  Предусмотреть возможность выдачи результата в годах, месяцах, днях, минутах, секундах (для первых двух вариантов ответ дробный). 
+    ///  Для переключения между вариантами желательно использовать переключатели (RadioButton).
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        private TimeSpan _tm_span = new TimeSpan();
+        private int _months = 0;
+        private int _days = 0;
+        Regex _regexDate = new Regex(@"^([0-2][0-9]|(3)[0-1])(\.)|(\/)|(\-)(((0)[0-9])|((1)[0-2]))(\.)|(\/)|(\-)\d{0,4}$");
+        private int _bounces = 0;
+        private int _max_bounces = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Text = "Calculate time span";
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timerChekAndRun.Interval = 10;
+            timerChekAndRun.Enabled = true;
+            timerChekAndRun.Start();
+        }
+
+        private void UpdateResult()
+        {
+            if (radioButtonYears.Checked)
+            {
+                textBoxResult.Text = $"{(_tm_span.Days / 365)} years , {_tm_span.Days % 365} days ({Math.Round(_tm_span.TotalDays / 365, 3)})";
+            }
+            else if (radioButtonMonths.Checked)
+            {
+                textBoxResult.Text = $"{_months} months {_days} days ({Math.Round(_tm_span.TotalDays / 30.4, 3)})";
+            }
+            else if (radioButtonDays.Checked)
+            {
+                textBoxResult.Text = $"{_tm_span.Days} days";
+            }
+            else if (radioButtonMinutes.Checked)
+            {
+                textBoxResult.Text = $"{Math.Round(_tm_span.TotalMinutes)} minutes";
+            }
+            else if (radioButtonSeconds.Checked)
+            {
+                textBoxResult.Text = $"{Math.Round(_tm_span.TotalSeconds)} seconds";
+            }
+        }
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            _tm_span = dateTimePickerEndDate.Value.Subtract(DateTime.Now);
+
+            _months = (dateTimePickerEndDate.Value.Month - DateTime.Now.Month) + (dateTimePickerEndDate.Value.Year - DateTime.Now.Year) * 12;
+
+            _days = (dateTimePickerEndDate.Value.Day - DateTime.Now.Day) >= 0 ? (dateTimePickerEndDate.Value.Day - DateTime.Now.Day) : 0;
+
+            if (dateTimePickerEndDate.Value < DateTime.Now)
+            {
+                _months += (dateTimePickerEndDate.Value.Day > DateTime.Now.Day && _months <= 0) ? 1 : 0;
+                _days = dateTimePickerEndDate.Value.Day <= DateTime.Now.Day ? (dateTimePickerEndDate.Value.Day - DateTime.Now.Day)
+                    : (DateTime.DaysInMonth(dateTimePickerEndDate.Value.Year, dateTimePickerEndDate.Value.Month + 1) - (dateTimePickerEndDate.Value.Day - DateTime.Now.Day));
+            }
+            else
+            {
+                _months -= (dateTimePickerEndDate.Value.Day < DateTime.Now.Day && _months > 0) ? 1 : 0;
+                _days = dateTimePickerEndDate.Value.Day >= DateTime.Now.Day ? (dateTimePickerEndDate.Value.Day - DateTime.Now.Day)
+                    : (DateTime.DaysInMonth(dateTimePickerEndDate.Value.Year, dateTimePickerEndDate.Value.Month - 1) - (DateTime.Now.Day - dateTimePickerEndDate.Value.Day));
+            }
+            UpdateResult();
+        }
+
+        private void radioButtonYears_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void radioButtonMonthes_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void radioButtonDays_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void radioButtonMinutes_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void radioButtonSeconds_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateResult();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_regexDate.IsMatch(textBoxInputEndDate.Text))
+                {
+                    DateTime _tmp_dt = DateTime.Parse(textBoxInputEndDate.Text);
+                    _tm_span = _tmp_dt.Subtract(DateTime.Now);
+                    _months = (_tmp_dt.Month - DateTime.Now.Month) + (_tmp_dt.Year - DateTime.Now.Year) * 12;
+
+                    _days = (_tmp_dt.Day - DateTime.Now.Day) >= 0 ? (_tmp_dt.Day - DateTime.Now.Day) : 0;
+
+                    if (_tmp_dt < DateTime.Now)
+                    {
+                        _months += (_tmp_dt.Day > DateTime.Now.Day && _months <= 0) ? 1 : 0;
+                        _days = _tmp_dt.Day <= DateTime.Now.Day ? (_tmp_dt.Day - DateTime.Now.Day)
+                            : (DateTime.DaysInMonth(_tmp_dt.Year, _tmp_dt.Month + 1) - (_tmp_dt.Day - DateTime.Now.Day));
+                    }
+                    else
+                    {
+                        _months -= (_tmp_dt.Day < DateTime.Now.Day && _months > 0) ? 1 : 0;
+                        _days = _tmp_dt.Day >= DateTime.Now.Day ? (_tmp_dt.Day - DateTime.Now.Day)
+                            : (DateTime.DaysInMonth(_tmp_dt.Year, _tmp_dt.Month - 1) - (DateTime.Now.Day - _tmp_dt.Day));
+                    }
+
+                    UpdateResult();
+                }
+            }
+            catch (Exception)
+            {
+                //none//
+            }
+        }
+
+        private void buttonShowInfo_Click(object sender, EventArgs e)
+        {
+            buttonShowInfo.Location = this.PointToClient(new Point(this.Location.X + this.Width - buttonShowInfo.Width - 10, this.Location.Y + 10 + buttonShowInfo.Height));
+            _bounces = 0;
+            MessageBox.Show("You can input some end date and calculate how many time left for that date as seconds ,minutes,days etc.", "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void timerChekAndRun_Tick(object sender, EventArgs e)
+        {
+            int margin = 25;
+            Random rnd = new Random();
+            int x = 0;
+            int y = 0;
+
+            Point pTL = new Point();
+            pTL = buttonShowInfo.PointToScreen(pTL);
+            pTL.X -= margin;
+            pTL.Y -= margin;
+
+            Point pBR = new Point();
+            pBR = buttonShowInfo.PointToScreen(pBR);
+            pBR.X += margin + buttonShowInfo.Width;
+            pBR.Y += margin + buttonShowInfo.Height;
+
+            if ((MousePosition.X > pTL.X && MousePosition.Y > pTL.Y)
+                && (MousePosition.X < pBR.X && MousePosition.Y < pBR.Y)
+                && _bounces < _max_bounces)
+            {
+                x = rnd.Next(this.Location.X + buttonShowInfo.Width, this.Location.X + this.Width - buttonShowInfo.Width - 10);
+                y = rnd.Next(this.Location.Y + buttonShowInfo.Height + 10, this.Location.Y + this.Height - buttonShowInfo.Height);
+                buttonShowInfo.Location = this.PointToClient(new Point(x, y));
+                _bounces++;
+            }
+        }
+    }
+}
